0795b30c6622516623b45ed87a3a66685b9451ba
Dimokat0 <Dmitriy.holius@fivesysdev.com>
2024-01-04 17:14:50 +0200

    Created new branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    endOfLine: 'CRLF',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
